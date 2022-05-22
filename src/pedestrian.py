13d32169e304969bde5c280ef42c604029f769c7
Palmieri Luigi (CR/AAS3) <pal2rng@RNG-C-00150.rng.de.bosch.com>
2022-05-22 22:00:34 +0200

    rm

--- src/pedestrian.py
+++
@@ -1,7 +0,0 @@
-class Pedestrian:
-    def __init__(self):
-        self.start_frame = []  # first time stamp frame when the pedestrian is detected
-        self.end_frame = [] # last time stamp frame when the pedestrian is detected
-        self.frames = []  # vector of time stamp frames when the pedestrian is detected
-        self.ped_id = [] # ID (number) of the pedestrian
-        self.trajectory = [] # trajectory of the pedestrian
